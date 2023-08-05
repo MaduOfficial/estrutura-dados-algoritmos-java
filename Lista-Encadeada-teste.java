ff2af8d35b58212a8c5d47466c1fe9a76c8af260
Madu <137858554+MaduOfficial@users.noreply.github.com>
2023-08-05 14:45:24 -0300

    Create Lista-Encadeada-teste.java

---
+++ Lista-Encadeada-teste.java
@@ -0,0 +1,14 @@
+package Teste;
+
+import Lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+
+	public static void main(String[] args) {
+	    ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+	}
+
+}
